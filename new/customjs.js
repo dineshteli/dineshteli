d1948267b8a23be8c9103537ed874d99a20e354c
dineshteli <66553788+dineshteli@users.noreply.github.com>
2020-10-19 00:13:29 +0530

    new project added

---
+++ new/customjs.js
@@ -0,0 +1,14 @@
+
+
+    // display loading gif while load
+    jQuery(document).ready(function() {
+    jQuery('#loading').fadeOut(2000);
+});
+
+  
+    // nav-bar collapsed toggler
+    // $(document).ready(function(){
+    //   $(".nav-link").click(function(){
+    //     $(".navbar-toggler").click();
+    //   });
+    // });
