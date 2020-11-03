9916bd3f2d97e9937bc2dbd9f37495a51a5532c1
dineshteli <66553788+dineshteli@users.noreply.github.com>
2020-11-03 22:33:55 +0530

    code updated

--- mygym/customjs.js
+++ mygym/customjs.js
@@ -7,21 +7,35 @@
 
 
     ///////////////// fixed menu on scroll for desktop
-if ($(window).width() > 100) {
-  $(window).scroll(function(){  
-     if ($(this).scrollTop() > 40) {
-        $('#navbar_top').addClass("fixed-top");
-        $('.navbar-color').css('background-color', 'rgba(10, 10, 10, 0.7)');
-        // add padding top to show content behind navbar
-        $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
-      }else{
-         $('#navbar_top').removeClass("fixed-top");
-        $('.navbar-color').css('background-color', 'black');
-         // remove padding top from body
-        $('body').css('padding-top', '0');
-      }   
-  });
-} // end if
+// if ($(window).width() > 100) {
+//   $(window).scroll(function(){  
+//      if ($(this).scrollTop() > 40) {
+//         $('#navbar_top').addClass("fixed-top");
+//         $('.navbar-color').css('background-color', 'rgba(10, 10, 10, 0.7)');
+//         // add padding top to show content behind navbar
+//         $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+//       }else{
+//          $('#navbar_top').removeClass("fixed-top");
+//         $('.navbar-color').css('background-color', 'black');
+//          // remove padding top from body
+//         $('body').css('padding-top', '0');
+//       }   
+//   });
+// } // end if
+
+
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > 40) {
+      $(".navbar-color").css("background" , "rgba(10, 10, 10, 0.7)");
+    }
+    else{
+      $(".navbar-color").css("background" , "black");    
+    }
+  })
+})
 
 
 
